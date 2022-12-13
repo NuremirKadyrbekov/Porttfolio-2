bba2e191552236aabb7d6c0b678a77e0f27126cb
NuremirKadyrbekov <118036760+NuremirKadyrbekov@users.noreply.github.com>
2022-12-14 00:05:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const menuBtn = document.querySelector('.menu__icon')
+const menu = document.querySelector('.menu__list')
+
+if (menuBtn && menu) {
+	menuBtn.addEventListener('click', () => {
+		menuBtn.classList.toggle('active')
+		menu.classList.toggle('active')
+	})
+
+	menu.querySelectorAll('a').forEach(link => {
+		link.addEventListener('click', () => {
+			menuBtn.classList.toggle('active')
+			menu.classList.toggle('active')
+		})
+	})
+}
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+	anchor.addEventListener('click', e => {
+		e.preventDefault();
+
+		const blockID = anchor.getAttribute('href').substring(1);
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
+})
+
+const createSelectedSection = (root) => {
+	const nav = root.querySelector('nav');
+
+	root.querySelectorAll('.observe').forEach(s => {
+		new IntersectionObserver((entries) => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					nav.querySelectorAll('a').forEach(link => link.classList.remove('active'))
+					let id = entry.target.getAttribute('id');
+					nav.querySelector(`a[href="#${id}"]`).classList.add('active');
+				}
+			})
+		}, {}).observe(s);
+	})
+}
+
+createSelectedSection(document.querySelector('#page'))
